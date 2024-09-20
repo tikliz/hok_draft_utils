3e0da58a37563b8413de65cf51aa6502c18683ad
tikliz <tik8lizz@gmail.com>
2024-09-20 02:23:45 -0300

    fixed extension issue

--- replace_links.py
+++ replace_links.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 def update_field_in_json(file_path, target_field, new_value):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -7,7 +8,8 @@ def update_field_in_json(file_path, target_field, new_value):
     if "data" in data and "heroList" in data["data"]:
         for item in data["data"]["heroList"]:
             if target_field in item:
-                item[target_field] = new_value.format(item["heroName"])
+                extension = Path(item["icon"]).suffix
+                item[target_field] = new_value.format(item["heroName"], extension)
     
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
@@ -17,7 +19,7 @@ def update_field_in_json(file_path, target_field, new_value):
 # Usage example
 file_path = './res/heros/hero-data-ptBr.json'
 target_field = 'icon'
-new_value = 'https://raw.githubusercontent.com/tikliz/hok_draft_utils/refs/heads/main/res/heros/Images/image_hero_head-{}.png'  # The new value you want to set
+new_value = 'https://raw.githubusercontent.com/tikliz/hok_draft_utils/refs/heads/main/res/heros/Images/image_hero_head-{}{}'  # The new value you want to set
 
 updated_data = update_field_in_json(file_path, target_field, new_value)
 print("Updated data:", updated_data)
