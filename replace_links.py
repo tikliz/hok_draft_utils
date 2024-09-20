6be9295131871d3d472a80558985efe5562ab5d8
tikliz <tik8lizz@gmail.com>
2024-09-20 00:58:14 -0300

    Edited links to temporarily use github, and added small script to edit the links to whatever

---
+++ replace_links.py
@@ -0,0 +1,23 @@
+import json
+
+def update_field_in_json(file_path, target_field, new_value):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    
+    if "data" in data and "heroList" in data["data"]:
+        for item in data["data"]["heroList"]:
+            if target_field in item:
+                item[target_field] = new_value.format(item["heroName"])
+    
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+    
+    return data
+
+# Usage example
+file_path = './res/heros/hero-data-ptBr.json'
+target_field = 'icon'
+new_value = 'https://raw.githubusercontent.com/tikliz/hok_draft_utils/refs/heads/main/res/heros/Images/image_hero_head-{}.png'  # The new value you want to set
+
+updated_data = update_field_in_json(file_path, target_field, new_value)
+print("Updated data:", updated_data)
